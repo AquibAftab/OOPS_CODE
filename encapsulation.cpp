dfc80df7573e552ae9f8fee731b111ddd7fe52ff
aaquibaftab <aaquibaftab7903@gmail.com>
2025-08-03 23:51:33 +0530

    added encaps file

---
+++ encapsulation.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std;
+
+ class A{
+    public:
+    int data;
+    int val;
+    public:
+    void solve(){
+        cout<<" This is an exmple of encapsulation"<<endl;
+    }
+ };
+
+int main(){
+    A a1;
+    a1.solve();
+}
